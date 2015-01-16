9bac561fe7611a16a9371a1584b74d1b7879dbf5
jaruk55011212147 <jaruk.mot@gmail.com>
2015-01-16 16:02:44 +0700

    tast1

---
+++ Frist.playground/section-1.swift
@@ -0,0 +1,24 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+let tutorialteam = 56
+let editorialtram = 23
+var totalTeam = tutorialteam + editorialtram
+
+totalTeam += 1
+let priceIntInferred = -19
+let priceIntExplicit:Int = -19
+
+let priceInferred = -19.99
+let princeExplicit:Double = -19.99
+
+let priceFloatInferred = -19.99
+let priceFloatExplicit:Float = -19.99
+
+let onSaleInferred = true
+let onSaleExplicit:Bool = false
+
+let nameInferred = "whoopie Cushion"
+let nameExplicit:String = "whoopie cushion"

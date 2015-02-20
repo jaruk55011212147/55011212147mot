bc6ff32c10864ab227c842a35ffb8e067b753a3a
jaruk55011212147 <jaruk.mot@gmail.com>
2015-02-20 17:32:44 +0700

    LABCOR IMAGE

--- tipCalculator2 .swift
+++
@@ -1,9 +0,0 @@
-//
-//  tipCalculator2 .swift
-//  TableView Menu
-//
-//  Created by iStudents on 2/13/15.
-//  Copyright (c) 2015 iStudents. All rights reserved.
-//
-
-import Foundation
---
+++ tipCalculator2/tipCalculator2/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  tipCalculator2
+//
+//  Created by iStudents on 2/13/15.
+//  Copyright (c) 2015 iStudents. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var tatal: UILabel!
+    
+    @IBOutlet weak var tax: UILabel!
+    
+    @IBOutlet weak var tatalTexField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
